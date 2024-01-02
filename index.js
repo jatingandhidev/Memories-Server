22db47a8637c4a5df97bf3d4b1dda62672beba07
Salvad0r14 <jatin05092001@gmail.com>
2024-01-02 18:10:14 +0530

    commit

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const PORT = process.env.PORT || 5000
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() =>
     app.listen(PORT, () => console.log(`server running on port ${PORT}`))
